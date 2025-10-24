87e1a77b244803a47a33e7641e18ba3a10405ac0
Manish Jha <manishkj2204@gmail.com>
2025-10-24 18:07:00 +0530

    initial commit

---
+++ src/test/java/com/codechallenge/wc/WcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codechallenge.wc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
