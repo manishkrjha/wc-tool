e5bb54998a7c245cd9dba26301380752b8c53199
Manish Jha <manishkj2204@gmail.com>
2025-10-27 23:43:01 +0530

    Added file path validation logic

---
+++ src/main/java/com/codechallenge/wc/service/FileValidator.java
@@ -0,0 +1,31 @@
+package com.codechallenge.wc.service;
+
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Component
+public class FileValidator {
+
+    public Path validateFilePath(String filePath){
+        if(filePath.isBlank()){
+            throw new IllegalArgumentException("No file is provided");
+        }
+
+        Path path = Path.of(filePath);
+
+        if(!Files.exists(path)){
+            throw new IllegalArgumentException("No such file exists at the location");
+        }
+
+        if(Files.isDirectory(path)){
+            throw new IllegalArgumentException("Invalid file name provided");
+        }
+        
+        return path;
+    }
+
+
+}
