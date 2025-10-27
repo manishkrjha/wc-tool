f0c08958efeb4687aff72ec96f724ab6a24083d4
Manish Jha <manishkj2204@gmail.com>
2025-10-28 00:42:05 +0530

    Added streams to process file

--- src/main/java/com/codechallenge/wc/WordCountCmd.java
+++ src/main/java/com/codechallenge/wc/WordCountCmd.java
@@ -1,17 +1,23 @@
 package com.codechallenge.wc;
 
 import com.codechallenge.wc.service.FileHandler;
+import com.codechallenge.wc.service.FileValidator;
 import org.springframework.shell.command.annotation.Command;
 import org.springframework.shell.command.annotation.Option;
 import org.springframework.shell.standard.ShellCommandGroup;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 @Command(command = "ccwc", description = "CLI tool to get the word cound of a text file")
 public class WordCountCmd {
 
     private final FileHandler fileHandler;
+    private final FileValidator fileValidator;
 
-    public WordCountCmd(FileHandler fileHandler) {
+    public WordCountCmd(FileHandler fileHandler, FileValidator fileValidator) {
         this.fileHandler = fileHandler;
+        this.fileValidator = fileValidator;
     }
 
     @Command
@@ -21,24 +27,25 @@ public class WordCountCmd {
             @Option(longNames = "w", shortNames = 'w', description = "outputs the number of words in a file") boolean w,
             @Option(longNames = "m", shortNames = 'm', description = "outputs the number of characters in a file") boolean m,
             @Option(description = "File path") String filePath
-    ){
+    ) throws IOException {
 
         StringBuilder sb = new StringBuilder();
+        Path path = fileValidator.validateFilePath(filePath);
 
         if(c){
-            sb.append(fileHandler.getByteCount(filePath));
+            sb.append("Byte count: ").append(fileHandler.getByteCount(path)).append("\n");
         }
 
         if(l){
-            sb.append(fileHandler.getLineCount(filePath));
+            sb.append("Line count: ").append(fileHandler.getLineCount(path)).append("\n");
         }
 
         if(w){
-            sb.append(fileHandler.getWordCount(filePath));
+            sb.append("Word count: ").append(fileHandler.getWordCount(path)).append("\n");
         }
 
         if(m){
-            sb.append(fileHandler.getCharCount(filePath));
+            sb.append("Char count: ").append(fileHandler.getCharCount(path)).append("\n");
         }
 
         return sb.toString();
--- src/main/java/com/codechallenge/wc/service/FileHandler.java
+++ src/main/java/com/codechallenge/wc/service/FileHandler.java
@@ -2,23 +2,54 @@ package com.codechallenge.wc.service;
 
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 @Service
 public class FileHandler {
 
-    public String getWordCount(String filePath){
-        return "";
+    private final String ERROR_MESSAGE = "Exception occurred while reading file: {}";
+
+    public String getWordCount(Path filePath){
+        try{
+            Stream<String> lines = Files.lines(filePath);
+
+            List<String> wordList = lines
+                    .flatMap(line -> Arrays.stream(line.split("\\s+")))
+                    .toList();
+
+            return Integer.toString(wordList.size());
+        } catch (IOException e) {
+            throw new RuntimeException(ERROR_MESSAGE, e);
+        }
     }
 
-    public String getLineCount(String filePath){
-        return "";
+    public String getLineCount(Path filePath){
+        try{
+            Stream<String> lines = Files.lines(filePath);
+            return Long.toString(lines.count());
+        } catch (IOException e) {
+            throw new RuntimeException(ERROR_MESSAGE, e);
+        }
     }
 
-    public String getCharCount(String filePath){
-        return "";
+    public String getCharCount(Path filePath){
+
+        try {
+            Stream<String> lines = Files.lines(filePath);
+            int charCount = lines.mapToInt(String::length).sum();
+            return Integer.toString(charCount);
+        } catch (IOException e) {
+            throw new RuntimeException(ERROR_MESSAGE, e);
+        }
     }
 
-    public String getByteCount(String filePath){
-        return "";
+    public String getByteCount(Path filePath) throws IOException {
+        return Long.toString(Files.size(filePath));
     }
 
 }
